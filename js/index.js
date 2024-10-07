c59ddccfb1911c4728ffff214cea0deff094e3ef
CarlosVQZ <jvazquez@shotsware.com>
2024-10-07 16:59:37 -0600

    avance en la responsividad

--- js/index.js
+++ js/index.js
@@ -2,13 +2,36 @@ $('.slick-produtcs').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 2,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 1000,
     centerMode: true,
     nextArrow: false,
     prevArrow: false,
     dots:true,
-    centerPadding: '60px',
+    //centerPadding: '0px',
+    responsive: [
+      {
+          /* de 480 para abajo */
+          breakpoint: 480,
+          settings: {
+              slidesToShow: 1
+          }
+      },
+      {
+          /* de 967 hacia abajo */
+          breakpoint: 967,
+          settings: {
+              slidesToShow: 2
+          }
+      },
+      {
+          /* de 3000 hacia abajo */
+          breakpoint: 3000,
+          settings: {
+              slidesToShow: 3
+          }
+      }
+  ]
   });
 
 $('.slick-tecnologia').slick({
@@ -19,9 +42,39 @@ $('.slick-tecnologia').slick({
   autoplaySpeed: 1000,
   centerMode: true,
   dots:true,
-  centerPadding: '60px',
+  //centerPadding: '60px',
   nextArrow: $('.next'),
   prevArrow: $('.prev'),
+  responsive: [
+    {
+        /* de 480 para abajo */
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1
+        }
+    },
+    {
+        /* de 967 hacia abajo */
+        breakpoint: 967,
+        settings: {
+            slidesToShow: 1
+        }
+    },
+    {
+        /* de 3000 hacia abajo */
+        breakpoint: 1500,
+        settings: {
+            slidesToShow: 2
+        }
+    },
+    {
+        /* de 3000 hacia abajo */
+        breakpoint: 3000,
+        settings: {
+            slidesToShow: 3
+        }
+    }
+]
 });
 
 
@@ -33,44 +86,4 @@ $(".js-video-button").modalVideo({
   }
 });
 
-
-// $('.slick-center').slick({
-//   autoplay: true,
-//   autoplaySpeed: 1000,
-//   infinite: true,
-//   centerMode: true,
-//   centerPadding: '60px',
-//   slidesToShow: 1,
-//   arrows: true,
-//   nextArrow: $('.next'),
-//   prevArrow: $('.prev'),
-//   responsive: [
-//       {
-//           /* de 480 para abajo */
-//           breakpoint: 480,
-//           settings: {
-//               slidesToShow: 1
-//           }
-//       },
-//       {
-//           /* de 967 hacia abajo */
-//           breakpoint: 967,
-//           settings: {
-//               slidesToShow: 2
-//           }
-//       },
-//       {
-//           /* de 3000 hacia abajo */
-//           breakpoint: 3000,
-//           settings: {
-//               slidesToShow: 3
-//           }
-//       }
-//   ]
-// });
-
-
-// const myboton = document.getElementById('myboton');
-// myboton.addEventListener('click', function(e) {
-//   myboton.classList.add('d-none');
-// });
+//document.getElementById('year').innerHTML = new Date().getFullYear();
